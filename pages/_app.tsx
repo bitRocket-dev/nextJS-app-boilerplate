f93c5e88c30c08c359bffa91d53c86797a5ec2da
Daniele Maione <115459945+danielemaione-bitrocketdev@users.noreply.github.com>
2023-03-30 12:24:24 +0200

    feat: adeed theme (#21)
    Refs #14

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,12 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
+import { ThemeProvider } from '../common/theme/Provider';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider>
+    <Component {...pageProps} />;
+  </ThemeProvider>
+);
 
 export default appWithTranslation(App);
