a70f610ca283fed06e56eec850829d1e143123bb
danielemaione-bitrocketdev <d.maione@bitrocket.dev>
2023-04-03 15:43:46 +0200

    feat: switch theme
    Refs #45

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/** @format */
+import '@emotion/react';
+
+declare module '*.md';
+declare module 'react-csv';
+declare module '@microlink/react';
+declare module '@emotion/react' {
+  export interface Theme {
+    background: string;
+    navbar: string;
+    text: string;
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,29 @@
 /* eslint-disable import/no-unresolved */
 
 import '@/styles/globals.css';
+import { useEffect, useState } from 'react';
+
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 
+import { SwitchTheme } from '../common/components-shared/SwitchTheme';
 import { ThemeProvider } from '../common/theme/Provider';
+import { utilityGetTheme } from '../common/utils/utilityGetTheme';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider>
-    <Component {...pageProps} />;
-  </ThemeProvider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
+
+  useEffect(() => {
+    const storageTheme = utilityGetTheme() || 'dark';
+    setTheme(storageTheme);
+  }, []);
+
+  return (
+    <ThemeProvider themeSelected={theme}>
+      <SwitchTheme theme={theme} setTheme={setTheme} />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
+};
 
 export default appWithTranslation(App);
