3948a1d6af0421b96c284e42632191069bcccd94
danielemaione-bitrocketdev <d.maione@bitrocket.dev>
2023-04-03 14:48:24 +0200

    feat: added script for create components
    Refs #33

---
+++ create-component.js
@@ -0,0 +1,152 @@
+#!/usr/bin/env node
+/** @format */
+
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+
+const COMPONENT_TYPES = {
+  'components.ui': 'UI component',
+  'components-shared': 'Shared component',
+  utils: 'Utility function',
+  hooks: 'Hook function',
+};
+
+function createComponent() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'componentName',
+        message: 'Enter component name:',
+        validate: function (input) {
+          if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+          else return 'Component name may only include letters, numbers, underscores and hashes.';
+        },
+      },
+      {
+        type: 'list',
+        name: 'componentType',
+        message: 'Select component type:',
+        choices: Object.values(COMPONENT_TYPES),
+      },
+      {
+        type: 'confirm',
+        name: 'hasTest',
+        message: 'Would you like to add this component to unit test?',
+      },
+    ])
+    .then(({ componentName, componentType, hasTest }) => {
+      const componentTypeKey = Object.keys(COMPONENT_TYPES).find(key => COMPONENT_TYPES[key] === componentType);
+      const dirPath = path.join(
+        process.cwd(),
+        componentTypeKey === 'components.ui'
+          ? 'common/components.ui'
+          : componentTypeKey === 'components-shared'
+          ? 'common/components-shared'
+          : componentTypeKey === 'utils'
+          ? 'common/utils'
+          : 'common/hooks',
+      );
+      const componentFilePath = path.join(dirPath, `${componentName}.tsx`);
+
+      if (fs.existsSync(componentFilePath)) {
+        console.error(`Component "${componentName}" already exists!`);
+        process.exit(1);
+      }
+
+      fs.mkdirSync(dirPath, { recursive: true });
+
+      const componentNameUpper = componentName.charAt(0).toUpperCase() + componentName.slice(1);
+
+      let componentContent = '';
+
+      if (componentTypeKey === 'components.ui') {
+        componentContent = `
+import { memo } from 'react';
+
+import styled from '@emotion/styled';
+
+      const Styled${componentNameUpper}Container = styled.div\({
+      /* Add your styles here */
+      \});
+            
+      interface Props {
+      label?: string;
+      /* Add your props here */
+      }
+            
+      export const ${componentNameUpper} = memo(({ label }: Props) =>  <Styled${componentNameUpper}Container>{label}</Styled${componentNameUpper}Container>
+        );
+        `;
+      } else if (componentTypeKey === 'components-shared') {
+        componentContent = `
+        import { memo } from 'react';
+
+        import styled from '@emotion/styled';
+        import { useTranslation } from 'next-i18next';
+
+              const Styled${componentNameUpper}Container = styled.div\({
+              /* Add your styles here */
+              \});
+                    
+              interface Props {
+              label?: string;
+              /* Add your props here */
+              }
+                    
+              export const ${componentNameUpper} = memo(({ label }: Props) => {
+                const { t } = useTranslation('common');
+                return (
+                  <Styled${componentNameUpper}Container>
+                  {t(label)}
+                  </Styled${componentNameUpper}Container>
+                );
+                },
+                );
+                `;
+      } else if (componentTypeKey === 'utility') {
+        componentContent = `export const ${componentNameUpper} = () => {
+          // Add your utility function here
+        };
+        `;
+      } else if (componentTypeKey === 'hook') {
+        componentContent = `export const ${componentNameUpper} = () => {
+                  // Add your hook function here
+                  const count = 0;
+                  return {
+                    // Add your return here
+                    count
+                  }
+                };
+                `;
+      }
+
+      fs.writeFileSync(componentFilePath, componentContent);
+
+      console.log(`Component "${componentName}" created successfully as "${componentType}" component!`);
+
+      if (hasTest) {
+        componentContent = `
+/** @format */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ${componentNameUpper} } from '../common/${componentTypeKey}/${componentName}';
+        
+        describe('${componentNameUpper}', () => {
+          test('renders without errors', () => {
+            const { getByText } = render(<${componentNameUpper} label="Click Me" />);
+            const componentElement = getByText('Click Me');
+            expect(componentElement).toBeInTheDocument();
+          });
+        });
+            
+`;
+        const componentFilePath = path.join('./_test_', `${componentName}.test.jsx`);
+        fs.writeFileSync(componentFilePath, componentContent);
+      }
+    });
+}
+
+createComponent();
