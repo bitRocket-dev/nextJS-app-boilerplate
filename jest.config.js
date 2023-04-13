84710095e1da4b1308d0e0a6ab3974b657d8ef57
Daniele Maione <115459945+danielemaione-bitrocketdev@users.noreply.github.com>
2023-04-13 15:51:08 +0200

    fix: fixed type bugs on jest (#61)
    Refs #60

--- jest.config.js
+++ jest.config.js
@@ -11,9 +11,9 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
